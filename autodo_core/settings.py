75506ac5183014d0a52baaf8455e2df07d108c04
tomasServer <20461973hola@gmail.com>
2026-08-08 23:47:21 -0400

    Corregida sintaxis de ALLOWED_HOSTS

--- autodo_core/settings.py
+++ autodo_core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-qhagf&@-(l@sf)1wyzn!)ul!3b2fm)##qy_vjol&^y++38ar#r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
